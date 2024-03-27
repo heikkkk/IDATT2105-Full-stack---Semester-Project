78ac45a357a8b8815eefb0db7b7b7dca4c96cf9e
Anders Høvik <andehovi@stud.ntnu.no>
2024-03-27 08:51:03 +0100

    Added CSS to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,8 +5,33 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <div class="container">
-    <RouterLink to="/about us">About us</RouterLink>
-    <RouterLink to="/terms and policy">Terms and policy</RouterLink>
-    <RouterLink to="/etc">Etc ...</RouterLink>
+    <div id="linksCon">
+    <RouterLink class="router" to="/about us">About us</RouterLink>
+    <RouterLink class="router" to="/terms and policy">Terms and policy</RouterLink>
+    <RouterLink class="router" to="/etc">Etc ...</RouterLink>
+    <p id="ref">@2024 IDATT2105, Icons by icon8</p>
+    </div>
+    <div id="centerPart">
+      <div class="imageBackground">
+        <div id="imageTwitter">
+        </div>
+      </div>
+      <div class="imageBackground">
+        <div id="imageInsta">
+        </div>
+      </div>
+      <div class="imageBackground">
+        <div id="imageFacebook">
+        </div>
+      </div>
+      <div id="leftPart">
+        <input id="form" placeholder="enter email" />
+        <button id="subscribeButton">Subscribe</button>
+      </div>
+    </div>
+
   </div>
+
+
+
 </template>
