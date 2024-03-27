96744d49a96966ac574ec093c7a396769590e2d0
Anders Høvik <andehovi@stud.ntnu.no>
2024-03-27 22:21:10 +0100

    Added components for playingView with images

---
+++ src/components/PlayGame/ImageContainer.vue
@@ -0,0 +1,17 @@
+<script setup>
+import "/src/assets/css/PlayGame/imageConteiner.css"
+</script>
+
+<template>
+<div class="imageSize">
+  <div class="imageCon">
+
+  </div>
+
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/PlayingView.vue
+++ src/views/PlayingView.vue
@@ -2,10 +2,12 @@
 
 import QuestionHeader from '@/components/PlayGame/QuestionHeader.vue'
 import Answer from '@/components/PlayGame/AnswerBody.vue'
+import ImageContainer from '@/components/PlayGame/ImageContainer.vue'
 </script>
 
 <template>
 <QuestionHeader></QuestionHeader>
+  <ImageContainer></ImageContainer>
   <Answer></Answer>
 </template>
 
