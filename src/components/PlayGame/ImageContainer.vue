da8c5b17acd5e3d87013ae207d74cef7b5e3aa23
Anders Høvik <andehovi@stud.ntnu.no>
2024-03-28 10:53:37 +0100

    Added component to contain images

--- src/components/PlayGame/ImageContainer.vue
+++ src/components/PlayGame/ImageContainer.vue
@@ -1,12 +1,12 @@
 <script setup>
+import "/src/assets/img/animal-pet-kitten-cat-feline-mammal-749376-pxhere.com.jpg"
 import "/src/assets/css/PlayGame/imageConteiner.css"
 </script>
 
 <template>
 <div class="imageSize">
-  <div class="imageCon">
-
-  </div>
+  <img class="imageCon" src="/src/assets/img/animal-pet-kitten-cat-feline-mammal-749376-pxhere.com.jpg"  alt="cats doing stuff">
+  
 
 
 </div>
