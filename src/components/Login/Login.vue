d3af5b76724fd577713b3aa4d56041e956cca498
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-03-29 13:08:39 +0100

    Add authentication check to login

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -3,14 +3,49 @@ import '../../assets/css/Login/login.css'
 import LogoTitle from '@/components/LogoTitle.vue'
 import { useRouter } from 'vue-router';
 import { useUserStore } from '@/stores/UserStore.js'
+import axios from 'axios'
+import { ref, computed } from 'vue'
 
 // TODO implement form validation
-const router = useRouter();
-const store = useUserStore()
+const router = useRouter()
+const userStore = useUserStore()
+
+const username = ref('')
+const password = ref('')
+
+let authenticationError = ref(false);
+
+const authConfig = computed(() => ({
+  auth: {
+    username: username.value,
+    password: password.value
+  }
+}))
+
+function parseResponse(response) {
+  userStore.setUsername(response.data.user_name)
+  userStore.setToken(response.data.access_token)
+}
+
+const postLoginCredentials = () => {
+  axios.post('http://localhost:8080/sign-in',{}, authConfig.value)
+  .then(response => {
+    parseResponse(response, userStore)
+    router.push('/discover')
+  })
+  .catch(error => {
+    //TODO show error message to the user
+    authenticationError.value = true
+    console.log("[LoginService::postLoginCredentials]")
+  })
+}
+
 const login = () => {
-  store.setUsername("Username") //Todo remove this testing line
-  router.push('/discover')
+  postLoginCredentials()
 };
+
+
+
 </script>
 
 <template>
@@ -18,8 +53,9 @@ const login = () => {
     <div class="login-container">
       <form class="login-form" @submit.prevent="login">
         <LogoTitle color="#ffffff"></LogoTitle>
-        <input class="username-input" type="text" placeholder="Username">
-        <input class="password-input" type="password" placeholder="Password">
+        <p v-if="authenticationError" class="error_msg">Incorrect username or password</p>
+        <input v-model="username" :class="authenticationError === false ? 'username-input' : 'error'" type="text" placeholder="Username">
+        <input v-model="password" :class="authenticationError === false ? 'password-input' : 'error'" type="password" placeholder="Password">
         <div class="login-button-container">
           <button class="login-button" type="submit">Login</button>
           <div class="signup-paragraph-container">
