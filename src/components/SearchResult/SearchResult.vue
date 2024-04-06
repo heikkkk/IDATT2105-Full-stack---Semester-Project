6bfb4bf4147c2698cfbc705786d499aad2fb8635
Jens Christian Aanestad <jenscaa@stud.ntnu.no>
2024-04-06 22:09:39 +0200

    Added styling on SearchResult component

--- src/components/SearchResult/SearchResult.vue
+++ src/components/SearchResult/SearchResult.vue
@@ -2,6 +2,7 @@
 import '../../assets/css/SearchResult/searchResult.css'
 import { useQuizStore } from '@/stores/QuizStore.js'
 import ResultLink from '@/components/SearchResult/ResultLink.vue'
+import { categoryMapper, getCategoryImage } from '@/services/DiscoverService.js'
 
 let resultKey = 0
 const results = useQuizStore().getSearchResults
@@ -20,7 +21,14 @@ defineExpose({
 <template>
   <div class="search-result-wrapper" :key="resultKey">
     <div class="search-result-container">
-      <ResultLink class="carousel-image" v-for="(item, index) in results" :key="index" :title="item.quizTitle" :id="item.quizId" :img='item.thumbnailFilepath' />
+      <ResultLink class="carousel-image" v-for="(item, index) in results"
+                  :key="index"
+                  :title="item.quizTitle"
+                  :id="item.quizId"
+                  :img='getCategoryImage(item.categoryId)'
+                  :category="categoryMapper[item.categoryId]"
+                  :author="item.author"
+      />
     </div>
   </div>
 </template>
