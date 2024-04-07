22de77ddbaf49fafa9d01f6174e439903e88ea6b
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-04-07 17:29:55 +0200

    Added test to check if carousels render inside Discover

---
+++ src/__tests__/Discover.test.js
@@ -0,0 +1,18 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, test } from 'vitest'
+import Discover from '@/components/Discover/Discover.vue'
+import Carousel from '@/components/Discover/Carousel.vue'
+
+describe('Discover', () => {
+
+  test('should render carousels correctly', () => {
+    const wrapper = mount(Discover)
+
+   let categoryQuizzes = wrapper.vm.categoryQuizzes
+    console.log(categoryQuizzes)
+    const userQuizCarousel = wrapper.findAllComponents(Carousel)[1]
+    expect(userQuizCarousel.props().content).toEqual(categoryQuizzes)
+  })
+
+
+})
